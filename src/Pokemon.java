bfd96a4c1243739748ca7e86c24f6da93bfc1890
mateusmoreiradev-code <mateus.moreira.dev@gmail.com>
2026-04-15 17:28:28 -0300

    feat: criação de classe pokemon

---
+++ src/Pokemon.java
@@ -0,0 +1,10 @@
+public class Pokemon {
+    protected int idPokemon;
+    protected int custoPokemon;
+    protected int ataquePokemon;
+    protected TipoElemental tipoPokemon;
+    protected double alcancePokemon;
+    protected double velocidadeDeAtaquePokemon;
+    protected List<Habilidades> habilidadesPokemon = new ArrayList<>();
+    protected int evolucaoPokemon;
+}

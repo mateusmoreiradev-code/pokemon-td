b18ca0e011c74e2d48d612cabbf5a89493c736ca
mateusmoreiradev-code <mateus.moreira.dev@gmail.com>
2026-04-17 19:45:18 -0300

    feat: construtor pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,5 +1,6 @@
 public class Pokemon {
     protected int idPokemon;
+    protected String nomePokemon;
     protected int custoPokemon;
     protected int ataquePokemon;
     protected TipoElemental tipoPokemon;
@@ -7,4 +8,23 @@ public class Pokemon {
     protected double velocidadeDeAtaquePokemon;
     protected List<Habilidades> habilidadesPokemon = new ArrayList<>();
     protected int evolucaoPokemon;
+
+    public Pokemon(int idPokemon, String nomePokemon, int custoPokemon, int ataquePokemon, TipoElemental tipoPokemon, double alcancePokemon, double velocidadeDeAtaquePokemon, List<Habilidades> habilidadesPokemon, int evolucaoPokemon){
+        this.idPokemon = idPokemon;
+        this.nomePokemon = nomePokemon;
+        this.custoPokemon = custoPokemon;
+        this.ataquePokemon = ataquePokemon;
+        this.tipoPokemon = tipoPokemon;
+        this.alcancePokemon = alcancePokemon;
+        this.velocidadeDeAtaquePokemon = velocidadeDeAtaquePokemon;
+        this.habilidadesPokemon = habilidadesPokemon;
+        this.evolucaoPokemon = evolucaoPokemon;
+    };
+
+    public String getNomePokemon() {
+        return nomePokemon;
+    }
+    public void setNomePokemon(String nomePokemon){
+        this.nomePokemon = nomePokemon;
+    }
 }
